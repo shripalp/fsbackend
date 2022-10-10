37228b2f5d957725867c12fa9792d5ad0596829c
shripal <shripalp@gmail.com>
2022-10-10 17:32:16 -0600

    3.14 phonebook database step 2

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+require("dotenv").config();
+const Phonebook = require("./models/phonebook");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const phonebook = require("./models/phonebook");
 const app = express();
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+
 app.use(express.json());
-app.use(requestLogger);
+
 //app.use(morgan("tiny"));
 
 app.use(
@@ -25,114 +22,96 @@ app.use(cors());
 
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Phonebook.find({}).then((persons) => response.json(persons));
 });
 
 app.get("/info", (request, response) => {
   const currentDate = new Date();
   const timeStamp = currentDate.toString();
-  const message = `<p>Phonebook has info for ${persons.length} people <br><br>${timeStamp} </p>`;
-  response.send(message);
+  Phonebook.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p><p>${timeStamp}</p>`
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => {
-    return person.id === id;
+  Phonebook.findById(request.param.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.statusMessage = "This api doesnot exist";
+      response.status(404).end();
+    }
   });
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.statusMessage = "This api doesnot exist";
-    response.status(404).end();
-  }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
-const generateId = () => {
-  let maxId;
-  do {
-    maxId = Math.floor(Math.random() * 10000000);
-  } while (persons.find((person) => person.id === maxId));
+// const generateId = () => {
+//   let maxId;
+//   do {
+//     maxId = Math.floor(Math.random() * 10000000);
+//   } while (persons.find((person) => person.id === maxId));
 
-  return maxId;
-};
+//   return maxId;
+// };
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name) {
-    //console.log(body.name);
-    return response.status(400).json({
-      error: "name missing",
-    });
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: "number missing",
-    });
-  } else if (
-    persons.includes(persons.find((person) => person.name === body.name))
-  ) {
-    //console.log(typeof body.name);
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  } else {
-    const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number,
-    };
+  const person = new Phonebook({
+    name: body.name,
+    number: body.number,
+  });
 
-    persons = persons.concat(person);
-
-    response.json(person);
-  }
-
-  //console.log(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
+
+//console.log(person);
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
